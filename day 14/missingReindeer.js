fce29678e38569a8ad65b2245bb671c2ee8a4f28
Fernando Ocaña Zarceño <fernando.ocana@autentia.com>
2021-12-14 20:51:21 +0100

    Day 14. Searching the missing item (Fix coercion)

--- day 14/missingReindeer.js
+++ day 14/missingReindeer.js
@@ -2,7 +2,7 @@ function missingReindeer(ids) {
     const match = Array(ids.length).fill('X')
     ids.forEach(id => match[id] = id)
     const idx = match.indexOf('X')
-    return idx != -1 ? idx : ids.length
+    return idx !== -1 ? idx : ids.length
 }
 
 module.exports = missingReindeer;
